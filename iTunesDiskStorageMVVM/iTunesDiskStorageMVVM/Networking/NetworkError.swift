8bf466c9b85262c5065d5f7525f946cd0a581e50
ronesta <gvozzzd123@gmail.com>
2025-01-25 22:46:05 +0300

    Initial commit

---
+++ iTunesDiskStorageMVVM/iTunesDiskStorageMVVM/Networking/NetworkError.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkError.swift
+//  iTunesDiskStorageMVVM
+//
+//  Created by Ибрагим Габибли on 25.01.2025.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+}
